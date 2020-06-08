9eb9b04feffe04e09589d044009176edd0352b1c
Eric Satoshi <satoshi.eric@gmail.com>
2020-06-08 18:38:49 -0300

    05-Array: arrayMetodos.js -> 05-Array: simulandoArrays.js

---
+++ 05-Array/02-arrayMetodos.js
@@ -0,0 +1,29 @@
+const pilotos = ['Vettel', 'Alonso', 'Raikkonen', 'Massa']
+
+pilotos.pop() // massa quebrou ocarro!
+console.log(pilotos)
+
+pilotos.push('Verstappen')
+console.log(pilotos)
+
+pilotos.shift() // remove o primeiro
+console.log(pilotos)
+
+pilotos.unshift('Hamilton')
+console.log(pilotos)
+
+// splice pode adicionar e remover elementos
+
+// adiconar elementos
+pilotos.splice(2, 0, 'Bottas', 'Massa')
+console.log(pilotos)
+
+// remover elementos
+pilotos.splice(3, 1) // massa quebrou :(
+console.log(pilotos)
+
+const algunsPilotos1 = pilotos.slice(2) // novo array
+console.log(algunsPilotos1)
+
+const algunsPilotos2 = pilotos.slice(1, 4)
+console.log(algunsPilotos2)
