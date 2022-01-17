349c633d4152aa5dacdba77f32c03d7b167c2960
Eric Satoshi <satoshi.eric@gmail.com>
2022-01-17 18:23:55 -0300

    Projeto galeria finalizado

---
+++ bootstrap/projeto-galeria/src/index.js
@@ -0,0 +1,10 @@
+// Arquivos SASS
+import './scss/index.scss'
+
+// Dependências
+import 'jquery'
+import 'bootstrap'
+
+// Meus arquivos javascript
+import './js/core/includes'
+import './js/plugins/cityButton'
