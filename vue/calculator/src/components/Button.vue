4e4cd24f219c6eccb2387d24e2006305320ccdf6
Eric Satoshi <satoshi.eric@gmail.com>
2022-02-11 19:15:13 -0300

    calculadora vue iniciada

---
+++ vue/calculator/src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="button">
+      0
+  </button>
+</template>
+
+<script>
+export default {
+    props: {
+        label: {},
+        operation: { type: Boolean },
+        double: {type: Boolean}
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/calculator/src/main/Calculator.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="calculator">
+      <Display />
+      <Button />
+      <Button />
+      <Button />
+  </div>
+</template>
+
+<script>
+import Display  from '../components/Display.vue'
+import Button   from '../components/Button.vue'
+
+export default {
+    components: { Button, Display}
+}
+</script>
+
+<style>
+    .calculator {
+        height: 320px;
+        width: 235px;
+        border-radius: 5px;
+        overflow: hidden;
+
+        display: grid;
+        grid-template-columns: repeat(4, 25%);
+        grid-template-rows: 1fr repeat(5, 48px);
+
+    }
+</style>
