54cc0378cf4b9a254fd33bdda8bc7048abdb91f1
Eric Satoshi <satoshi.eric@gmail.com>
2022-02-21 18:57:48 -0300

    Calculadora finalizada

--- vue/calculator/src/main/Calculator.vue
+++ vue/calculator/src/main/Calculator.vue
@@ -1,31 +1,103 @@
 <template>
   <div class="calculator">
-      <Display />
-      <Button />
-      <Button />
-      <Button />
+    <Display :value="displayValue" />
+    <Button label="AC" @onClick="clearMemory" triple />
+    <Button label="/" @onClick="setOperation" operation />
+    <Button label="7" @onClick="addDigit" />
+    <Button label="8" @onClick="addDigit" />
+    <Button label="9" @onClick="addDigit" />
+    <Button label="*" @onClick="setOperation" operation />
+    <Button label="4" @onClick="addDigit" />
+    <Button label="5" @onClick="addDigit" />
+    <Button label="6" @onClick="addDigit" />
+    <Button label="-" @onClick="setOperation" operation />
+    <Button label="1" @onClick="addDigit" />
+    <Button label="2" @onClick="addDigit" />
+    <Button label="3" @onClick="addDigit" />
+    <Button label="+" @onClick="setOperation" operation />
+    <Button label="0" @onClick="addDigit" double />
+    <Button label="." @onClick="addDigit" />
+    <Button label="=" @onClick="setOperation" operation />
   </div>
 </template>
 
 <script>
-import Display  from '../components/Display.vue'
-import Button   from '../components/Button.vue'
+import Display from "../components/Display.vue";
+import Button from "../components/Button.vue";
 
 export default {
-    components: { Button, Display}
-}
+  data: function () {
+    return {
+      displayValue: "0",
+      clearDisplay: false,
+      operation: null,
+      values: [0, 0],
+      current: 0,
+    };
+  },
+  components: { Button, Display },
+  methods: {
+    clearMemory() {
+      Object.assign(this.$data, this.$options.data());
+    },
+    setOperation(operation) {
+      if (this.current === 0) {
+        this.operation = operation;
+        this.current = 1;
+        this.clearDisplay = true;
+      } else {
+        const equals = operation === "=";
+        const currentOperation = this.operation;
+        try {
+          this.values[0] = eval(
+            `${this.values[0]} ${currentOperation} ${this.values[1]}`
+          );
+          if (isNaN(this.values[0]) || !isFinite(this.values[0])) {
+            this.clearMemory();
+            return;
+          }
+        } catch (e) {
+          this.emit("onError", e);
+        }
+
+        this.values[1] = 0;
+
+        this.displayValue = this.values[0];
+        this.operation = equals ? null : operation;
+        this.current = equals ? 0 : 1;
+        this.clearDisplay = !equals;
+      }
+    },
+    addDigit(n) {
+      if (n === "." && this.displayValue.includes(".")) return;
+      const clearDisplay = this.displayValue === "0" || this.clearDisplay;
+
+      const currentValue = clearDisplay ? "" : this.displayValue;
+      const displayValue = currentValue + n;
+
+      this.displayValue = displayValue;
+      this.clearDisplay = false;
+      this.values[this.current] = displayValue;
+
+      if (n !== ".") {
+        const i = this.current;
+        const newValue = parseFloat(displayValue);
+        this.values[i] = newValue;
+      }
+    },
+  },
+};
 </script>
 
 <style>
-    .calculator {
-        height: 320px;
-        width: 235px;
-        border-radius: 5px;
-        overflow: hidden;
+.calculator {
+  height: 320px;
+  width: 235px;
+  border-radius: 5px;
+  overflow: hidden;
 
-        display: grid;
-        grid-template-columns: repeat(4, 25%);
-        grid-template-rows: 1fr repeat(5, 48px);
-
-    }
+  display: grid;
+  grid-template-columns: repeat(4, 25%);
+  grid-template-rows: 1fr repeat(5, 48px);
+}
 </style>
